e8be8a9023ddbcf22d6e10877cab408c3cd765e5
SEOA7777 <pleahmacaka@gmail.com>
2023-03-31 10:51:28 +0900

    feat(SlashCommand): build and deploy (#2)

--- src/custom/Client.ts
+++ src/custom/Client.ts
@@ -1,12 +1,15 @@
-import { Client as ClientJS, ClientOptions, Collection, Message } from "discord.js"
+import { Client as ClientJS, ClientOptions, Collection, CommandInteraction, Integration, Interaction, Message } from "discord.js"
 import { readdirSync } from "fs"
 import { join } from "path"
 import { PrefixCommand } from "../interfaces/PrefixCommand"
+import { SlashCommand } from "../interfaces/SlashCommand"
 import Logger from "@pleahmacaka/logger"
 
 export class Client extends ClientJS {
 
     public prefixCommands = new Collection<string, PrefixCommand>()
+
+    public slashCommands = new Collection<string, SlashCommand>()
 
     public defaultPrefix = "!"
 
@@ -34,6 +37,29 @@ export class Client extends ClientJS {
             const command = this.prefixCommands.get(args[0])
             if (command) command.execute(message, args)
         })
+        this.on("interactionCreate", (interaction: Interaction) => {
+            if (!interaction.isChatInputCommand()) return
+            const command = this.slashCommands.get(interaction.commandName)
+
+            if (!command) return
+
+            command.execute(interaction as CommandInteraction)
+        })
+        this.on("interactionCreate", (interaction: Interaction) => {
+            if (!interaction.isAutocomplete()) return
+            const command = this.slashCommands.get(interaction.commandName)
+            try {
+                if (!command?.autocomplete) return
+                command.autocomplete(interaction)
+            } catch (error) {
+                console.error(error)
+            }
+        })
+        // this.on("interactionCreate", (interaction: Interaction) => {
+        //     if (!interaction.isAutocomplete()) return
+        //     const command = this.slashCommands.get(interaction.commandName
+            
+        // })
     }
 
 }
